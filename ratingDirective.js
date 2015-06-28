4071e5e2b4c6c038b41d015582d5f6c6a23ac059
Lionel DEMANOU <lioneldemanou@yahoo.fr>
2015-06-28 04:50:18 +0200

    now can initialize star with default value

--- ratingDirective.js
+++ ratingDirective.js
@@ -1,57 +1,50 @@
+angular.module( 'hmrating.directives', [])
 
-app.filter('range', function() {
-  return function(input, total) {
-    total = parseInt(total);
-    for (var i=1; i<=total; i++)
-      input.push(i);
-    return input;
-  };    
-})
-
-app.directive('hmrating',function(){
-  return {
+.directive('hmrating',function(){
+    return {
         restrict: 'EA',
-        scope: { 
+        scope: {
             //@ reads the attribute value, = provides two-way binding, & works with functions
             hmupto:"@",
             hmid:"@",
             hmcolor:'@',
             rate:'=',
-            setrating : '&', 
+            setrating : '&',
             mouseover : '&',
             mouseleave : '&'
         },
-        templateUrl: 'rating.html',
+        templateUrl: '/views/admin/blocs/rating.html',
         link : function(scope,element,attr){
-            scope.setrating(0);
-        },       
+            scope.setrating(scope.rate | 0);
+        },
         controller: function($scope){
-    
+
             $scope.setrating = function(r){
                 ratingEffect(r);
                 $scope.rate = r;
             };
-            
+
             $scope.mouseover = function(r){
                 ratingEffect(r);
             }
-            
+
             $scope.mouseleave = function(){
-                ratingEffect($scope.rate);               
+                ratingEffect($scope.rate);
             }
 
             function ratingEffect(r){
                 for(var i=0;i<=r;i++)
-                    angular.element(document.querySelector("#"+$scope.hmid+i)).removeClass("fa-star-o"); 
+                    angular.element(document.querySelector("#"+$scope.hmid+i)).removeClass("fa-star-o");
 
-                for(var i=0;i<=r;i++)
-                    angular.element(document.querySelector("#"+$scope.hmid+i)).addClass("fa-star"); 
+                  for(var i=0;i<=r;i++)
+                    angular.element(document.querySelector("#"+$scope.hmid+i)).addClass("fa-star");
 
                 for(var i=r+1;i<=$scope.hmupto;i++){
-                    angular.element(document.querySelector("#"+$scope.hmid+i)).removeClass("fa-star"); 
-                    angular.element(document.querySelector("#"+$scope.hmid+i)).addClass("fa-star-o"); 
+                    angular.element(document.querySelector("#"+$scope.hmid+i)).removeClass("fa-star");
+                    angular.element(document.querySelector("#"+$scope.hmid+i)).addClass("fa-star-o");
                 }
+
             }
         }
-   };
+    };
 });
